adfa0a0aff9dfef10915e5d33682e25d25214995
Dima Lishnovskiy <lishnovskiy.dima@gmail.com>
2024-10-25 19:45:32 +0300

    format

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <TheHalloween msg="Welcome to Your Vue.js App"/>
+  <TheHalloween msg="Welcome to Your Vue.js App" />
 </template>
 
 <script setup>
-import TheHalloween from './components/TheHalloween.vue'
-
+import TheHalloween from "./components/TheHalloween.vue";
 </script>
-
